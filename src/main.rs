5b8d135aa01baaa06d95c5fc82663467a4231f4d
Carlton-Perkins <Carltonrp52@gmail.com>
2024-12-30 13:07:17 -0500

    added poor mans http server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
-use rocket::fs::relative;
-use rocket::fs::FileServer;
-use rocket::fs::Options;
+use std::path::PathBuf;
+
+use rocket::fs::NamedFile;
+use rocket::get;
+use rocket::routes;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build().mount(
         "/blog",
-        FileServer::new(relative!("quartz/public"), Options::Index),
+        routes![file_server], // FileServer::new(relative!("quartz/public"), Options::Index),
     );
 
     Ok(rocket.into())
+}
+
+#[get("/<file..>")]
+pub async fn file_server(file: PathBuf) -> Option<NamedFile> {
+    let mut path = PathBuf::from("quartz/public").join(file);
+    if path.is_dir() {
+        path.push("index.html");
+    }
+
+    println!("try path: {:?}", path);
+    if let Some(file) = NamedFile::open(path.clone()).await.ok() {
+        return Some(file);
+    }
+
+    path.set_extension("html");
+    println!("try path: {:?}", path);
+    if let Some(file) = NamedFile::open(path).await.ok() {
+        return Some(file);
+    }
+
+    let path = PathBuf::from("quartz/public/index.html");
+    if let Some(file) = NamedFile::open(path).await.ok() {
+        return Some(file);
+    }
+
+    None
 }
